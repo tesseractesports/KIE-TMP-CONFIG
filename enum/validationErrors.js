5b858da9a1d24c56f6856a9bb298c4305abcad16
VigneshTess <vignesh.varikolu@tesseractesports.com>
2024-08-27 12:17:11 +0530

    added common enums from vendor registration flow

---
+++ enum/validationErrors.js
@@ -0,0 +1,49 @@
+const emailError = "Please enter a valid email address";
+const dateOfBirthError = "Please click a valid DOB";
+const firstNameError = "Please enter a valid first name";
+const lastNameError = "Please enter a valid last name";
+const designationError = "Please enter a valid designation";
+const phoneError = "Please enter a valid number";
+
+const companyNameError = "please enter valid company name";
+const companyMarketNameError = "please enter valid market name";
+const countryError = "please select country";
+const stateError = "please select state";
+const cityError = "please select city";
+const companyEmailError = "please select valid email";
+const comapnyPhoneError = "please select valid number";
+const websiteError = "please select valid website";
+const logoError = "please select valid logo";
+const companyBioError = "Please enter a valid company bio";
+
+const legalNameError = "please enter valid legal name";
+const entityTypeError = "please enter valid entity";
+const panError = "please enter valid pan details";
+const gstError = "please enter valid gst details";
+const incorporationDateError = "please enter valid date";
+const incorporationCertificateError = "please upload valid certificate";
+
+module.exports = {
+    emailError,
+    dateOfBirthError,
+    firstNameError,
+    lastNameError,
+    designationError,
+    phoneError,
+    companyNameError,
+    companyMarketNameError,
+    countryError,
+    stateError,
+    cityError,
+    companyEmailError,
+    comapnyPhoneError,
+    websiteError,
+    logoError,
+    companyBioError,
+    legalNameError,
+    entityTypeError,
+    panError,
+    gstError,
+    incorporationCertificateError,
+    incorporationDateError
+}
